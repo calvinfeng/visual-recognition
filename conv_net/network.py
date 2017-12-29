84465a05696e397874725824b0c64f5657c7f8ab
Calvin Feng <cfeng@fetchrobotics.com>
2017-12-29 00:42:58 -0800

    Added grad computations in the network with combined gates

--- conv_net/network.py
+++ conv_net/network.py
@@ -4,10 +4,11 @@ import numpy as np
 
 
 class Network(object):
-    def __init__(self, hidden_dims, input_dim=32*32*3, num_classes=10, weight_scale=1e-3, reg=0.0):
+    def __init__(self, hidden_dims, input_dim=32*32*3, num_classes=10, weight_scale=1e-3, reg=0.0, use_batchnorm=False):
         self.reg = reg
         self.num_layers = len(hidden_dims) + 1
         self.dtype = np.float32
+        self.use_batchnorm = use_batchnorm
         self.params = dict()
         self.gates = dict()
 
@@ -46,9 +47,72 @@ class Network(object):
         if mode == 'test':
             return scores
 
+        loss, grad_input = self._softmax(scores, y)
+        # Think of scores is an input to the softmax loss, so the gradient returned from _softmax is the gradient of the
+        # input, i.e. grad_score
+
+        layer = 1
+        while layer <= self.num_layers:
+            weight = self.params['W' + str(layer)]
+            loss += 0.5 * self.reg * np.sum(weight * weight)
+            layer += 1
+
+        grads = dict()
+        layer = self.num_layers
+        while layer > 0:
+            gate = self.gates[str(layer)]
+            if layer == self.num_layers:
+                grad_input, grads['W' + str(layer)], grads['b' + str(layer)] = gate.backward_pass(grad_input)
+                grads['W' + str(layer)] += self.reg * self.params['W' + str(layer)]
+            else:
+                if self.use_batchnorm:
+                    print "Using batchnorm, TO BE IMPLEMENTED"
+                else:
+                    grad_input, grads['W' + str(layer)], grads['b' + str(layer)] = gate.backward_pass(grad_input)
+                    grads['W' + str(layer)] += self.reg * self.params['W' + str(layer)]
+            layer -= 1
+
+        return loss, grads
+
+    def _softmax(self, x, y):
+        """Computes the loss and gradient for softmax classification
+
+        Args:
+            x: Input data, of shape (N, C) where x[i, j] is the score for jth class for the ith input
+            y: Vector of labels, of shape (N,) where y[i] is the label for x[i] and 0 <= y[i] < C
+
+        Returns:
+            loss: Scalar value of the loss
+            grad_x: Gradient of the loss with respect to x
+        """
+
+        shifted_logits = x - np.max(x, axis=1, keepdims=True)
+        Z = np.sum(np.exp(shifted_logits), axis=1, keepdims=True)
+        log_probs = shifted_logits - np.log(Z)
+
+        N = x.shape[0]
+        loss = -np.sum(log_probs[np.arange(N), y]) / N
+
+        probs = np.exp(log_probs)
+        grad_x = probs.copy()
+        grad_x[np.arange(N), y] -= 1
+        grad_x /= N
+
+        return loss, grad_x
+
 
 if __name__ == "__main__":
+    N = 100
+    num_classes = 10
     hidden_dims = [100, 100, 100, 100]
-    rand_input = np.random.randn(100, 32, 32, 3)
+
+    rand_inputs = np.random.randn(N, 32, 32, 3)
+    rand_labels = np.random.randint(num_classes, size=(N,))
+
     network = Network(hidden_dims)
-    print network.loss(rand_input)
+    print rand_labels
+    scores =  network.loss(rand_inputs)
+    loss, grads = network.loss(rand_inputs, rand_labels)
+
+    print "Scores: %s" % str(scores.shape)
+    print "Loss: %s and Grads has shape %s" % (loss, str(grads.keys()))
--- demo_net/softmax_loss_net.py
+++ demo_net/softmax_loss_net.py
@@ -29,40 +29,45 @@ class SoftmaxLossNetwork(object):
             self.params['W1'] -= learning_rate * grads['W1']
 
         return iters, loss_hist, acc_hist
-    
+
     def _forward_prop(self, x):
         acts = dict()
-        
+
         # (N x input_dim)(input_dim x hidden_dim)
-        acts['theta1'] = np.dot(x, self.params['W1']) 
-        
+        acts['theta1'] = np.dot(x, self.params['W1'])
+
         # (N x hidden_dim)
         acts['a1'] = 1 / (1 + np.exp(-acts['theta1']))
-        
+
         # (N x hidden_dim)(hidden_dim x output_dim)
         acts['theta2'] = np.dot(acts['a1'], self.params['W2'])
-        
+
         # Exponential scores can become very big numbers, in order to ensure numerical stability, we need to apply the
-        # following mathematical trick (shifting by max exponential score.)
-        acts['a2'] = np.exp(acts['theta2']) - np.max(acts['theta2'])
-        acts['a2'] = acts['a2'] / np.sum(acts['a2'], axis=1, keepdims=True)
-        
+        # following mathematical trick (shifting by max activation score.)
+        shifted_logits = acts['theta2'] - np.max(acts['theta2'], axis=1, keepdims=True)
+        Z = np.sum(np.exp(shifted_logits), axis=1, keepdims=True)
+
+        log_probs = shifted_logits - np.log(Z) # Just to avoid taking log of zero(s) by using a simple log trick.
+        acts['a2'] = np.exp(log_probs)
+        # acts['a2'] = np.exp(acts['theta2'])
+        # acts['a2'] = acts['a2'] / np.sum(acts['a2'], axis=1, keepdims=True)
+
         return acts
-    
+
     def _loss(self, acts, y):
         loss = 0
         N = len(y)
         for idx, val in enumerate(y):
             # val is the classification 0, 1, 2 etc...
             # idx is the ith example we are classifying
-            prob = acts['a2'][idx][val] 
+            prob = acts['a2'][idx][val]
             loss += -1 * np.log(max(prob, 1e-100)) # Prevent taking log(0) which gives infinity
-            
+
         return loss / N
-    
+
     def _backward_prop(self, x, y, acts):
         grads = dict()
-        
+
         # Theta 2 is equivalent to the score that we propagate toward our softmax layer. We taking derivative of the score with respect to
         # cross entropy loss here
         N = len(y)
@@ -72,23 +77,23 @@ class SoftmaxLossNetwork(object):
 
         # (hidden_dim x N)(N x output_dim)
         grads['W2'] = np.dot(acts['a1'].T, grads['theta2'])
-        
+
         # (N x output_dim)(output_dim x hidden_dim)
-        grads['a1'] = np.dot(grads['theta2'], self.params['W2'].T) 
-        
+        grads['a1'] = np.dot(grads['theta2'], self.params['W2'].T)
+
         # (N x hidden_dim)
-        grads['theta1'] = grads['a1'] * ((1 - acts['a1']) * acts['a1']) 
-        
+        grads['theta1'] = grads['a1'] * ((1 - acts['a1']) * acts['a1'])
+
         # (input_dim x N)(N x hidden_dim)
-        grads['W1'] = np.dot(x.T, grads['theta1']) 
-        
+        grads['W1'] = np.dot(x.T, grads['theta1'])
+
         return grads
-    
+
     def _num_grads(self, x, y, h=1e-5):
         num_grads = dict()
         for param_name in self.params:
             num_grads[param_name] = np.zeros_like(self.params[param_name])
-            
+
             it = np.nditer(self.params[param_name], flags=['multi_index'], op_flags=['readwrite'])
             while not it.finished:
                 # Suppose our loss function is a f(p) and p is the param vector
@@ -99,37 +104,37 @@ class SoftmaxLossNetwork(object):
                 self.params[param_name][midx] = p + h
                 acts = self._forward_prop(x)
                 fp_plus_h = self._loss(acts, y)
-                
+
                 # Evaluate loss function at p - h
                 self.params[param_name][midx] = p - h
                 acts = self._forward_prop(x)
                 fp_minus_h = self._loss(acts, y)
-                
+
                 # Restore original value
                 self.params[param_name][midx] = p
-                
+
                 # Slope
                 num_grads[param_name][midx] = (fp_plus_h - fp_minus_h) / (2 * h)
-                
+
                 it.iternext()
-    
-        return num_grads        
-    
+
+        return num_grads
+
     def gradient_check(self, x, y, h=1e-5):
         acts = self._forward_prop(x)
         grads = self._backward_prop(x, y, acts)
         num_grads = self._num_grads(x, y, h)
         err, param_count = 0, 0
-        
+
         for param_name in self.params:
             it = np.nditer(self.params[param_name], flags=['multi_index'], op_flags=['readwrite'])
             while not it.finished:
                 midx = it.multi_index
-                err += (np.abs(num_grads[param_name][midx] - grads[param_name][midx]) / 
-                    max(np.abs(num_grads[param_name][midx]), np.abs(grads[param_name][midx]))) 
+                err += (np.abs(num_grads[param_name][midx] - grads[param_name][midx]) /
+                    max(np.abs(num_grads[param_name][midx]), np.abs(grads[param_name][midx])))
                 param_count += 1
                 it.iternext()
-        
+
         return err / param_count
 
 
@@ -139,18 +144,18 @@ if __name__ == "__main__":
     input_dim, hidden_dim, output_dim = xtr.shape[1], 5, 3
     network = SoftmaxLossNetwork(input_dim, hidden_dim, output_dim)
     print 'Performing gradient check %s' % network.gradient_check(xtr, ytr)
-    
+
     iters, loss_hist, acc_hist = network.train(xtr, ytr, learning_rate=1e-1)
-    
+
     plt.subplot(2, 1, 1)
     plt.plot(iters, loss_hist)
     plt.title('Loss vs Iterations')
-    
+
     print loss_hist[0]
     print loss_hist[len(loss_hist) - 1]
-    
+
     plt.subplot(2, 1, 2)
     plt.plot(iters, acc_hist)
     plt.title('Accuracy vs Iterations')
-    
+
     plt.show()
